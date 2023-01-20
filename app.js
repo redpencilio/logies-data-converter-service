2cc5a8321f4432ef6e22bcd7dad20005c60a0a4d
Erika Pauwels <erika.pauwels@gmail.com>
2023-01-20 13:11:30 +0100

    Refactor data source loading and translations mapping

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ import concat from 'concat';
 import request from 'request';
 import loadSources from './lib/data-sources';
 import publish from './lib/publication';
-import tasks from './config/tasks';
+import { loadTasksFromConfig } from './lib/task';
+
+const tasks = loadTasksFromConfig();
 
 /** Schedule cron job */
 const cronFrequency = process.env.CRON_PATTERN || '0 0 2 * * *';
@@ -17,11 +19,8 @@ new CronJob(cronFrequency, function() {
   request.post('http://localhost/conversion-tasks');
 }, null, true);
 
-
-const outputDirectory = process.env.OUTPUT_DIRECTORY || '/output';
-
 // Static hosting of TTL files to LOAD in triple store
-[outputDirectory, '/app/static'].forEach(folder => {
+['/output', '/app/static'].forEach(folder => {
   app.use(express.static(folder, {
     index: false,
     setHeaders(res, path, stat) {
@@ -47,17 +46,11 @@ const convert = async function() {
 
     console.log('Start mapping');
     const taskPromises = tasks.map(async function(task) {
-      const input = await fs.readFile(task.inputFile, 'utf8');
-      const records = JSON.parse(input);
-      const ttl = task.mapper(records);
-      await fs.outputFile(task.outputFile, ttl);
-
-      const status = { title: task.title, inputFile: task.inputFile, count: records.length };
-      console.log(JSON.stringify(status));
-      return status;
+      const result = await task.execute();
+      console.log(JSON.stringify(result));
     });
 
-    const taskStatuses = await Promise.all(taskPromises);
+    await Promise.all(taskPromises);
 
     // const taskOutputs = tasks.map(t => t.outputFile);
     // const outputFileId = uuid();
@@ -71,5 +64,6 @@ const convert = async function() {
   } catch(e) {
     console.error('Something went wrong during the conversion');
     console.error(e.message || e);
+    console.trace(e);
   }
 };
